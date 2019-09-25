b8b2d59d088a68a1a1f519d40d611c149c9a0ca0
Sergio <seranmoreno500@gmail.com>
2019-09-25 21:09:40 +0200

    Removing the blogs and adding them to Google Drive.
    read READ ME for more info

--- DWES - Desarrollo Web en Entorno Servidor/Tema 1/Blog de cocina/wordpress/bin/wp.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-"C:/xampp/php/php" "C:/xampp/apps/wordpress/bin/wp-cli.phar" --path="C:/xampp/apps/wordpress/htdocs" %*
--- DWES - Desarrollo Web en Entorno Servidor/Tema 1/Blog de cocina/wordpress/htdocs/wp-content/themes/misrem/template-parts/content-none.php
+++
@@ -1,7 +0,0 @@
-<article <?php post_class(); ?>>
-	<div class="post__content">
-		<h3 class="post__title">
-			<?php _e( 'Sorry, but there is no content to display.', 'misrem' ); ?>
-		</h3>
-	</div>
-</article>
